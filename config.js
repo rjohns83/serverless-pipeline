ce672c2c37a81db03d41c2fc69a117509f52013f
rjohns83 <50498048+rjohns83@users.noreply.github.com>
2019-05-14 15:56:30 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_Ud3iOQ2xS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5us88s1v8ar3gbu08jcgfi6hc2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'http://rebaterjprogram.s3-website-us-east-1.amazonaws.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
